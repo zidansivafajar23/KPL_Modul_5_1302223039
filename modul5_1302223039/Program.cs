4091a71a945fd77bd6381ce8ac7b282c4358807b
MuhammadZidan <zidansivafajar23@gmail.com>
2024-03-21 11:01:15 +0700

    menambahkan class SimpleDataBase

--- modul5_1302223039/Program.cs
+++ modul5_1302223039/Program.cs
@@ -1,7 +1,43 @@
-﻿internal class Program
+﻿public class SimpleDataBase
+{
+    private List<T> storedData = new List<T>();
+    private List<DateTime> inputDates = new List<DateTime>();
+
+    public SimpleDataBase() 
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void addNewData(T data) 
+    {
+        this.storedData.Add(data);
+        imputDate.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+
+    }
+}
+public class Penjumlahan
+{
+
+    public void JumlahTigaAngka<T>(T x, T y, T z)
+    {
+        dynamic hasil = x;
+        dynamic hasil2 = y;
+        dynamic hasil3 = z;
+        dynamic hasil_akhir = hasil + hasil2 + hasil3;
+        Console.WriteLine($"Hasil penjumlahan dari {x} + {y} + {z} = {hasil_akhir}");
+    }
+}
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Penjumlahan jumlah = new Penjumlahan();
+        jumlah.JumlahTigaAngka<long>(13, 02, 22);
     }
 }
